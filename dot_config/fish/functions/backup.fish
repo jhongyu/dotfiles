adf4851b63a664a30d1ea018015f9d4f8db49a4a
jihongyu <jhy172691619@gmail.com>
2024-07-30 11:35:03 +0800

    Update .config/fish/functions/backup.fish

--- dot_config/fish/functions/backup.fish
+++ dot_config/fish/functions/backup.fish
@@ -1,4 +1,3 @@
 function backup
-  set -fx RESTIC_PASSWORD (bw get item 7fb38c5d-4301-4ae9-96bf-b1bd002e972a | jq '.notes')
   restic -r rclone:onedrive:Database --exclude='.stfolder' backup ~/Database --skip-if-unchanged
 end
