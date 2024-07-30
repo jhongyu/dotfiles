7cd3bd2c07336bef6d0791fcdd3f7ee456181795
jihongyu <jhy172691619@gmail.com>
2024-07-30 12:43:34 +0800

    Update .config/fish/functions/backup.fish

--- dot_config/fish/functions/backup.fish
+++ dot_config/fish/functions/backup.fish
@@ -1,3 +1,3 @@
 function backup
-  restic -r rclone:onedrive:Database --exclude='.stfolder' backup ~/Database --skip-if-unchanged --password-file=~/restic_pwd
+  restic -r rclone:onedrive:Database --exclude='.stfolder' backup ~/Database --skip-if-unchanged --password-file ~/restic_pwd
 end
